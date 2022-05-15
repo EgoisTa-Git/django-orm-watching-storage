26dc8ed0a4b45f8e8d1858c4f5b9323aaa736350
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-05-15 11:59:13 +0300

    Refactor variable names

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,16 +4,16 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-    workers_in_storage = Visit.objects.filter(leaved_at=None)
-    non_closed_visits = []
-    for worker in workers_in_storage:
-        non_closed_visit = {
-                'who_entered': worker.passcard.owner_name,
-                'entered_at': worker.entered_at,
-                'duration': format_duration(worker.get_duration()),
+    non_closed_visits = Visit.objects.filter(leaved_at=None)
+    serialized_non_closed_visits = []
+    for visit in non_closed_visits:
+        serialized_visit = {
+                'who_entered': visit.passcard.owner_name,
+                'entered_at': visit.entered_at,
+                'duration': format_duration(visit.get_duration()),
             }
-        non_closed_visits.append(non_closed_visit)
+        serialized_non_closed_visits.append(serialized_visit)
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': serialized_non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
